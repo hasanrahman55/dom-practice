511dec2df8204fcaa3725b031cb3e660bb401c84
Hasan Rahman <hasanrahman33605@gmail.com>
2025-02-25 14:59:59 +0600

    date element added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timeElement = document.getElementById("time");
 const dateElement = document.getElementById("date");
 
-function getTimeElement() {
+function updateClock() {
   const now = new Date();
   const hours = now.getHours() % 12 || 12;
   const minutes = now.getMinutes().toString().padStart(2, "0");
@@ -9,7 +9,15 @@ function getTimeElement() {
   const ampm = now.getHours() >= 12 ? "PM" : "AM";
 
   timeElement.textContent = `${hours}:${minutes}:${seconds}:${ampm}`;
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  dateElement.textContent = now.toLocaleDateString(undefined, options);
 }
 
-setInterval(getTimeElement, 1000);
+setInterval(updateClock, 1000);
 getTimeElement();
