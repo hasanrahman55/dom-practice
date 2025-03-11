bb96b57f7bd4f57239a7369c3a654311ecd4ef03
Hasan Rahman <hasanrahman33605@gmail.com>
2025-03-11 14:05:49 +0600

    date and time ui

---
+++ pages/date-time/script.js
@@ -0,0 +1,23 @@
+const timeElement = document.getElementById("time");
+const dateElement = document.getElementById("date");
+
+function dateAndTime() {
+  const date = new Date();
+
+  const hours = date.getHours() % 12 || 12;
+
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  const ampm = date.getDate() > 12 ? "PM" : "AM";
+
+  timeElement.textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
+  dateElement.textContent = date.toLocaleDateString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+}
+
+setInterval(dateAndTime, 1000);
